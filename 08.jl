7c276de51ed610aaa59206b2c7439d5bad5dae20
Raphael Reismueller <raphael@reismueller.at>
2021-12-08 22:15:20 +0100

    eighth day part one

---
+++ 08.jl
@@ -0,0 +1,13 @@
+input = map(readlines("08.input")) do i
+    data = split.(split(i, " | "), ' ')
+    (patterns=data[1], output=data[2])
+end
+
+# part 1
+function count_p1(line; n_count=[2, 3, 4, 7])
+    return count(x->(x in n_count), length.(line.output))
+end
+
+sum(count_p1.(input))
+
+# part 2
